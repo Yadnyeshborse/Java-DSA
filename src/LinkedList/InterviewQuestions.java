a45aed37dda0a0bb8d100acdaa03dfd68065f4fc
Yadnyesh Borse <yadvborse2711@gmail.com>
2025-01-20 00:42:43 +0530

    updated linked list

--- src/LinkedList/InterviewQuestions.java
+++ src/LinkedList/InterviewQuestions.java
@@ -66,6 +66,7 @@ public class InterviewQuestions {
 
     //reverse in two poinst 1 2 3 4 5 result 1 2 3 2 5
     // Reverse a portion of the linked list between two positions
+    //https://leetcode.com/problems/reverse-linked-list-ii/description/
     public ListNode reverseBetween(ListNode head, int left, int right) {
         if (left == right) {
             return head; // No need to reverse if left == right
@@ -104,6 +105,217 @@ public class InterviewQuestions {
         newEnd.next = current; // Connect the reversed portion to the rest of the list
         return head;
     }
+//_________________________________________________________________________________________________
+    //Palindrome Linked List
+    //https://leetcode.com/problems/palindrome-linked-list/description/
+
+
+    class Solution {
+        public boolean isPalindrome(ListNode head) {
+            ListNode mid=middleNode(head);
+            ListNode headSecond=reverseList(mid);
+            ListNode reversehead=headSecond;
+
+            while(head!=null && headSecond!=null){
+                if(head.val!=headSecond.val){
+                    reverseList(reversehead); // Restore the original list before returning
+                    return false;
+                }
+                head=head.next;
+                headSecond=headSecond.next;
+
+            }
+            reverseList(reversehead);
+            return true;
+
+        }
+
+        public ListNode middleNode(ListNode head) {
+            ListNode s = head;
+            ListNode f = head;
+
+            while (f != null && f.next != null) {
+                s = s.next;
+                f = f.next.next;
+            }
+            return s;
+        }
+        public ListNode reverseList(ListNode head) {
+            if (head == null) {
+                return head;
+            }
+            ListNode prev = null;
+            ListNode present = head;
+            ListNode next = present.next;
+
+            while (present != null) {
+                present.next = prev;
+                prev = present;
+                present = next;
+                if (next != null) {
+                    next = next.next;
+                }
+            }
+            return prev;
+        }
+
+    }
+
+    //____________________________________________________________________________________________
+    //https://leetcode.com/problems/reorder-list/description/
+    class Solution {
+        public void reorderList(ListNode head) {
+            if(head==null||head.next==null){
+                return;
+            }
+            ListNode mid=middleNode(head);
+
+            ListNode headSecond=reverseList(mid);
+            ListNode headFirst=head;
+
+            while (headFirst != null && headSecond != null) {
+                ListNode temp = headFirst.next;
+                headFirst.next = headSecond;
+                headFirst = temp;
+
+                temp = headSecond.next;
+                headSecond.next = headFirst;
+                headSecond = temp;
+            }
+
+            if (headFirst != null) {
+                headFirst.next = null;
+            }
+
+        }
+
+        public ListNode middleNode(ListNode head) {
+            ListNode s = head;
+            ListNode f = head;
+
+            while (f != null && f.next != null) {
+                s = s.next;
+                f = f.next.next;
+            }
+            return s;
+        }
+        public ListNode reverseList(ListNode head) {
+            if (head == null) {
+                return head;
+            }
+            ListNode prev = null;
+            ListNode present = head;
+            ListNode next = present.next;
+
+            while (present != null) {
+                present.next = prev;
+                prev = present;
+                present = next;
+                if (next != null) {
+                    next = next.next;
+                }
+            }
+            return prev;
+        }
+    }
+
+    //__________________________________________________________________________________
+
+    //https://leetcode.com/problems/reverse-nodes-in-k-group/
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k <= 1 || head == null) {
+            return head;
+        }
+
+        ListNode current = head;
+        ListNode prev = null;
+
+        int length = getLength(head);
+        int count = length / k;
+        while (count > 0) {
+            ListNode last = prev;
+            ListNode newEnd = current;
+
+            ListNode next = current.next;
+            for (int i = 0; current != null && i < k; i++) {
+                current.next = prev;
+                prev = current;
+                current = next;
+                if (next != null) {
+                    next = next.next;
+                }
+            }
+
+            if (last != null) {
+                last.next = prev;
+            } else {
+                head = prev;
+            }
+
+            newEnd.next = current;
+
+            prev = newEnd;
+            count--;
+        }
+        return head;
+
+    }
+    public int getLength(ListNode head) {
+        ListNode node = head;
+        int length = 0;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        return length;
+    }
+
+    //__________________________________________________________________________________________________
+
+    // https://www.geeksforgeeks.org/reverse-alternate-k-nodes-in-a-singly-linked-list/
+    public ListNode reverseAlternateKGroup(ListNode head, int k) {
+        if (k <= 1 || head == null) {
+            return head;
+        }
+
+        // skip the first left-1 nodes
+        ListNode current = head;
+        ListNode prev = null;
+
+        while (current != null) {
+            ListNode last = prev;
+            ListNode newEnd = current;
+
+            // reverse between left and right
+            ListNode next = current.next;
+            for (int i = 0; current != null && i < k; i++) {
+                current.next = prev;
+                prev = current;
+                current = next;
+                if (next != null) {
+                    next = next.next;
+                }
+            }
+
+            if (last != null) {
+                last.next = prev;
+            } else {
+                head = prev;
+            }
+
+            newEnd.next = current;
+
+            // skip the k nodes
+            for (int i = 0; current != null && i < k; i++) {
+                prev = current;
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    //_____________________________________________________________________________________________
+
 
 
     public static void main(String[] args) {
